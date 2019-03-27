ceca1ff0755d9c711d532759767465d867f4616b
igor <perrofeofilms@gmail.com>
2019-03-27 13:12:11 -0600

    -

---
+++ src/pages/Blog.vue
@@ -0,0 +1,21 @@
+<template>
+  <Layout>
+    <div v-for="edge in $page.posts.edges" :key="edge.node.id">
+      {{ edge.node.id }}
+      {{ edge.node.title }}
+    </div>    
+  </Layout>
+</template>
+
+<page-query>
+query Posts {
+  posts: Posts {
+    edges {
+      node { 
+        id
+        title
+      }
+    }
+  }
+}
+</page-query>
